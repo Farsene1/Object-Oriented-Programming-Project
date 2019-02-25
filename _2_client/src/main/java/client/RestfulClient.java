0e2d355a3a0f4eed8266e88e6e5425a423924dd6
Florentin <F.Arsene@student.tudelft.nl>
2019-02-25 18:29:57 +0100

    with bugs MAVEN SETUP

---
+++ _2_client/src/main/java/client/RestfulClient.java
@@ -0,0 +1,29 @@
+package client;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class RestfulClient {
+    RestTemplate restTemplate;
+
+    public RestfulClient(){
+        restTemplate = new RestTemplate();
+    }
+    /**
+     * get Entity
+     */
+    public void getEntity() {
+        System.out.println("Beginning /GET request!");
+        String getUrl = "http://localhost:8080/get?id=1&name=Florentin&age=19";
+        ResponseEntity<User> getResponse = restTemplate.getForEntity(getUrl, User.class); // User user = restTemplate.getForObject(getUrl, User.class); WHICH ONE IS BETTER? // DIFFERENT RESPONSES
+        System.out.println(getResponse.toString());
+    }
+
+    public void postEntity(User user){
+        System.out.println("Beginning /POST request");
+        String postUrl = "http://localhost:8080/post";
+        ResponseEntity<String> postResponse = restTemplate.postForEntity(postUrl, user, String.class);
+        System.out.println("Response for Post Request: " + postResponse.getBody());
+    }
+
+}
