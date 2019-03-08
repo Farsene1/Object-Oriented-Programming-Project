cce9c0d41c5753402dbe2652bbdc4183209c793b
Florentin <F.Arsene@student.tudelft.nl>
2019-03-08 22:05:24 +0100

    changes

--- _1_server/src/main/java/hello/User.java
+++ _1_server/src/main/java/hello/User.java
@@ -2,6 +2,7 @@ package hello;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @Table(name = "users")
 @Entity
@@ -14,9 +15,11 @@ public class User implements Serializable {
 
 	private String username;
 	private String hash;
-
-	@OneToOne
-	private Activity activity;
+	private Integer foodFootprint;
+	private Integer waterFootprint;
+	private Integer transportFootprint;
+	private Integer polarScore;
+	private Date date;
 
 	public User(String username, String hash){
 		this.username = username;
@@ -37,6 +40,46 @@ public class User implements Serializable {
 
 	public void setHash(String hash) {
 		this.hash = hash;
+	}
+
+	public Integer getFoodFootprint() {
+		return foodFootprint;
+	}
+
+	public void setFoodFootprint(Integer foodFootprint) {
+		this.foodFootprint = foodFootprint;
+	}
+
+	public Integer getWaterFootprint() {
+		return waterFootprint;
+	}
+
+	public void setWaterFootprint(Integer waterFootprint) {
+		this.waterFootprint = waterFootprint;
+	}
+
+	public Integer getTransportFootprint() {
+		return transportFootprint;
+	}
+
+	public void setTransportFootprint(Integer transportFootprint) {
+		this.transportFootprint = transportFootprint;
+	}
+
+	public Integer getPolarScore() {
+		return polarScore;
+	}
+
+	public void setPolarScore(Integer polarScore) {
+		this.polarScore = polarScore;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
 	}
 
 	@Override
