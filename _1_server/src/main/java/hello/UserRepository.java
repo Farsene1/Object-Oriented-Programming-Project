ea5510b1b99b736a01537db5b0aa994a54c0e4ed
Florentin <F.Arsene@student.tudelft.nl>
2019-03-09 09:21:22 +0100

    query updated

--- _1_server/src/main/java/hello/UserRepository.java
+++ _1_server/src/main/java/hello/UserRepository.java
@@ -1,7 +1,15 @@
 package hello;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+    @Query(value = "SELECT * from users", nativeQuery = true)
+    public List<User> findAllUsers();
+
+    @Query(value = "SELECT * from users where username ->> ?1", nativeQuery = true)
+    public User findUserByUsername(String username);
 }
