ac70ea2b17dd66cccfc883f2da473ac2b0ad5002
Florentin <F.Arsene@student.tudelft.nl>
2019-03-10 10:46:27 +0100

    faulty tests
    the mocking in the RestfulClient class is not successfully done

--- _2_client/src/test/java/RestfulClientTest.java
+++ _2_client/src/test/java/RestfulClientTest.java
@@ -3,15 +3,16 @@ import classes.User;
 import hello.GreetingController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RestfulClientTest {
 
-    @MockBean
-    private GreetingController controller;
+    @Autowired
+    private GreetingController controller = Mockito.mock(GreetingController.class);
 
     private RestfulClient client;
 
@@ -21,17 +22,26 @@ public class RestfulClientTest {
     }
 
     @Test
-    public void getTest(){
+    public void getTest(){ // WHY DOESN"T THIS WORK ?????????? problem with mocking the class
+        Mockito.when(controller.greeting("Florentin","abcdef")).thenReturn(new hello.User("admin","root"));
         ResponseEntity<User> rs = client.getEntity();
         System.out.println(rs.getBody().toString());
-        assertEquals(rs.getBody().getUsername(), "Florentin");
-        assertEquals(rs.getBody().getHash(),"abcdef");
+        assertEquals("Florentin",rs.getBody().getUsername());
+        assertEquals("abcdef",rs.getBody().getHash());
     }
 
     @Test
     public void postTest(){
+        Mockito.when(controller.postMethod(new hello.User("Florentin","123abc"))).thenReturn("/POST failed");
         ResponseEntity<String> rs = client.postEntity(new User("Florentin","123abc"));
         assertEquals(rs.getStatusCode().value(), 200);
-        assertEquals(rs.getBody(), "/POST successful");
+        assertEquals("/POST failed", rs.getBody());
+    }
+
+    @Test
+    public void loginTest(){
+        Mockito.when(controller.login(new hello.User("admin","root"))).thenReturn("YES");
+        String res = client.login(new User("admin","root"));
+        assertEquals("NEGATIVE",res);
     }
 }
