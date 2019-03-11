aa1ce05cef4f0be1435c2df2561f66d2406d3e6d
Florentin Arsene <F.Arsene@student.tudelft.nl>
2019-03-11 20:41:25 +0100

    editing pom

--- _1_server/src/main/java/hello/ActivityController.java
+++ _1_server/src/main/java/hello/ActivityController.java
@@ -1,49 +1,49 @@
-package hello;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * this is the restcontroller class that controls the received activities.
- */
-@RestController
-public class ActivityController {
-
-    /**
-     * autowiring the repository.
-     */
-    @Autowired
-    ActivityRepository activityRepository;
-
-    /**
-     *
-     * @param activity
-     * @return response to the client
-     */
-    @RequestMapping("/activity/send/meal")
-    public String getMeal(@RequestBody Activity activity){
-
-    }
-
-    /**
-     *
-     * @param activity
-     * @return response to the client
-     */
-    @RequestMapping("/activity/send/water")
-    public String getWater(@RequestBody Activity activity){
-
-    }
-    
-    /**
-     *
-     * @param activity
-     * @return response to the client
-     */
-    @RequestMapping("/activity/send/transport")
-    public String getTransport(@RequestBody Activity activity){
-
-    }
-}
+//package hello;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * this is the restcontroller class that controls the received activities.
+// */
+//@RestController
+//public class ActivityController {
+//
+//    /**
+//     * autowiring the repository.
+//     */
+//    @Autowired
+//    ActivityRepository activityRepository;
+//
+//    /**
+//     *
+//     * @param activity
+//     * @return response to the client
+//     */
+//    @RequestMapping("/activity/send/meal")
+//    public String getMeal(@RequestBody Activity activity){
+//
+//    }
+//
+//    /**
+//     *
+//     * @param activity
+//     * @return response to the client
+//     */
+//    @RequestMapping("/activity/send/water")
+//    public String getWater(@RequestBody Activity activity){
+//
+//    }
+//
+//    /**
+//     *
+//     * @param activity
+//     * @return response to the client
+//     */
+//    @RequestMapping("/activity/send/transport")
+//    public String getTransport(@RequestBody Activity activity){
+//
+//    }
+//}
