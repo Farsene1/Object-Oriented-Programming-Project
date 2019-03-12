80258055e751f05729f5d124bc54bc7155beeb91
Hashim Kaim <hashimkarim168@gmail.com>
2019-03-12 02:12:23 +0100

    Started Styling homepage (the sample.fxml was just for planning)

--- _2_client/src/main/java/client/Home.java
+++ _2_client/src/main/java/client/Home.java
@@ -1,10 +1,10 @@
 package client;
 
 import classes.RestfulClient;
-import classes.User;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -52,14 +52,21 @@ public static void showHome(Stage window){
 
     Label Welcome = new Label("Welcome to the homepage!");
 
+    ImageView img= new ImageView("https://drive.google.com/uc?id=12Bowa9WczV-WVRlgH-zsZcdcSQyMa2nn");
+    img.setStyle("-fx-pref-width: 350; -fx-pref-height: 350; -fx-padding: 25");
     //Add everything to grid
-    leftmenu.getChildren().addAll(Welcome, mycarbonButton, statisticsButton, leaderboardButton, quitButton);
+    leftmenu.getChildren().addAll(Welcome,img , mycarbonButton, statisticsButton, leaderboardButton, quitButton);
     bp.setLeft(leftmenu);
     bp.setStyle("-fx-background-image: url('https://i.pinimg.com/originals/36/f7/3d/36f73d2a6d91981d5a3aa644d897d467.jpg');");
     Scene scene = new Scene(bp, 1920, 1015);
 
+    leaderboardButton.setStyle("-fx-pref-width: 400;-fx-pref-height: 70;-fx-font-size: 42");
+    mycarbonButton.setStyle("-fx-pref-width: 400;-fx-pref-height: 70;-fx-font-size: 42");
+    quitButton.setStyle("-fx-pref-width: 400;-fx-pref-height: 70;-fx-font-size: 42");
+    statisticsButton.setStyle("-fx-pref-width: 400;-fx-pref-height: 70;-fx-font-size: 42");
     leftmenu.setMinWidth(400);
     leftmenu.setStyle("-fx-font-size: 18pt; -fx-background-color: white; -fx-padding: 10px;");
+    Welcome.setStyle("-fx-padding: 20,0,0,0; -fx-alignment: center; -fx-font-size: 30");
         window.setScene(scene);
         window.setMaximized(true);
         window.show();
