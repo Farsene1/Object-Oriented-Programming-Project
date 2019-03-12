aab61e0c97270719900920a49d735a2ae1f1aae5
Hashim Kaim <hashimkarim168@gmail.com>
2019-03-12 02:11:28 +0100

    Started Styling signup page

--- _2_client/src/main/java/client/Signup.java
+++ _2_client/src/main/java/client/Signup.java
@@ -8,9 +8,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.FileNotFoundException;
@@ -83,8 +85,17 @@ public class Signup {
 
         });
 
+        ImageView img= new ImageView("https://drive.google.com/uc?id=12Bowa9WczV-WVRlgH-zsZcdcSQyMa2nn");
+        img.setFitWidth(350);
+        img.setFitHeight(350);
+        img.setStyle("-fx-padding: 10");
+        VBox vbox = new VBox();
+        usernameLabel.setStyle("-fx-padding: 0 20 0 40");
+        passwordLabel.setStyle("-fx-padding: 0 20 0 40");
+
         //Add everything to grid
         grid.getChildren().addAll(usernameLabel, usernameInput, passwordLabel, passwordInput,passwordInput2, register);
+        vbox.getChildren().addAll(img,grid);
 
         Pane test1=new Pane();
         Pane test2=new Pane();
@@ -95,15 +106,15 @@ public class Signup {
         test3.setMinHeight(200);
         test4.setMinHeight(200);
 
-        bp.setCenter(grid);
+        bp.setCenter(vbox);
         bp.setLeft(test1);
         bp.setRight(test2);
         bp.setTop(test3);
         bp.setBottom(test4);
 
         bp.setStyle("-fx-background-image: url('https://i.pinimg.com/originals/36/f7/3d/36f73d2a6d91981d5a3aa644d897d467.jpg');");
-        grid.setStyle("-fx-background-color: rgba(255,255,255, 0.4); -fx-alignment: top-center; -fx-font-size: 17pt");
-
+        vbox.setStyle("-fx-background-color: rgba(255,255,255, 0.4); -fx-alignment: top-center; -fx-font-size: 17pt");
+        vbox.setPadding(new Insets(20,0,40,0));
 
         Scene scene = new Scene(bp, 1920, 1015);
         grid.setPadding(new Insets(100,0,0,0));
