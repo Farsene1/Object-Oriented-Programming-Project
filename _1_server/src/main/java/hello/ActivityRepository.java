510b009b82fb4a6e6df1ba769f1d4b052b242f34
julianholle <48092782+julianholle@users.noreply.github.com>
2019-03-12 21:33:35 +0100

    .

--- _1_server/src/main/java/hello/ActivityRepository.java
+++ _1_server/src/main/java/hello/ActivityRepository.java
@@ -1,11 +1,7 @@
 package hello;
 
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ActivityRepository extends CrudRepository<Activity, Integer> {
