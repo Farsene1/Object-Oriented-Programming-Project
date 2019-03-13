13b7e30e3e9430f1f049ecebe973ff376fe75b61
Sayf <sayfgmaul@gmail.com>
2019-03-13 19:25:26 +0100

    test change

--- _2_client/src/test/java/ControllerTest.java
+++ _2_client/src/test/java/ControllerTest.java
@@ -1,6 +1,6 @@
-import classes.Controller;
 import org.junit.Test;
 
+import static classes.Controller.checkEmail;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,8 +11,8 @@ public class ControllerTest {
     public void checkEmailTest(){
         String email1 = "sfss";
         String email2= "sayfgmaul@gmail.com";
-        assertEquals(Controller.checkEmail(email1), false);
-        assertEquals(Controller.checkEmail(email2), true);
-        assertTrue(Controller.checkEmail("email@email.com"));
+        assertEquals(checkEmail(email1), false);
+        assertEquals(checkEmail(email2), true);
+        assertTrue(checkEmail("email@email.com"));
     }
 }
