48005212be2ab39d724954ea570b700d595b12fe
Stelios Avgousti <s.avgousti@student.tudelft.nl>
2019-03-14 14:53:29 +0100

    adding stuff

--- _2_client/src/main/java/client/FootPrint.java
+++ _2_client/src/main/java/client/FootPrint.java
@@ -5,50 +5,38 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class FootPrint {
 
-    public static void showOptions(Stage window) {
-        RestfulClient restfulClient = new RestfulClient();
-        restfulClient.getEntity();
-        window.setTitle("My Footprint");
+    public static void showOptions(GridPane grid, Scene scene) {
 
-        BorderPane bp= new BorderPane();
-        VBox optionsMenu = new VBox();
+       // window.setTitle("My Footprint");
 
-        //Quit
-        Button quitButton = new Button("Home");
-        GridPane.setConstraints(quitButton, 0, 4);
-        quitButton.setOnAction(e -> {
-            boolean result = ConfirmBox.display("Confirm", "Return back to home page?");
-            if (result) {
-                Home.showHome(window);
-            }
 
-        });
+        VBox optionsMenu= new VBox();
+
         //My Food footprint
         Button Food = new Button("Add a meal!");
-        GridPane.setConstraints(Food, 1, 0);
+
+        GridPane.setConstraints(Food, 4, 1);
         Food.setOnAction(e -> {
             String food = addItemBox.addMeal("Food Footprint", "Select the meal type you wish to add.");
         });
 
         //My Transport footprint
         Button Transport = new Button("Add a journey!");
-        GridPane.setConstraints(Transport, 2, 0);
+        GridPane.setConstraints(Transport, 5, 1);
         Transport.setOnAction(e -> {
             AlertBox.display("This is an error message", "To Be Implemented");
         });
 
-        //My Food footprint
+        //My Water footprint
         Button Water = new Button("Add water use!");
-        GridPane.setConstraints(Water, 3, 0);
+        GridPane.setConstraints(Water, 6, 1);
         Water.setOnAction(e -> {
             AlertBox.display("This is an error message", "To Be Implemented");
         });
@@ -59,21 +47,21 @@ public class FootPrint {
         img.setStyle("-fx-pref-width: 350; -fx-pref-height: 350; -fx-padding: 25");
 
         //Add everything to grid
-        optionsMenu.getChildren().addAll(Welcome, img, Food, Transport, Water, quitButton);
-        bp.setCenter(optionsMenu);
-        bp.setStyle("-fx-background-image: url('https://i.pinimg.com/originals/36/f7/3d/36f73d2a6d91981d5a3aa644d897d467.jpg');");
-        Scene scene = new Scene(bp, 1920, 1080);
+        optionsMenu.getChildren().addAll(  Food, Transport, Water);
+        grid.getChildren().add(optionsMenu);
+      //  bp.setStyle("-fx-background-image: url('https://i.pinimg.com/originals/36/f7/3d/36f73d2a6d91981d5a3aa644d897d467.jpg');");
+      //  Scene scene = new Scene(bp, 1920, 1080);
 
         Food.setStyle("-fx-pref-width: 400;-fx-pref-height: 70;-fx-font-size: 42");
         Transport.setStyle("-fx-pref-width: 400;-fx-pref-height: 70;-fx-font-size: 42");
-        quitButton.setStyle("-fx-pref-width: 400;-fx-pref-height: 70;-fx-font-size: 42");
+       // quitButton.setStyle("-fx-pref-width: 400;-fx-pref-height: 70;-fx-font-size: 42");
         Water.setStyle("-fx-pref-width: 400;-fx-pref-height: 70;-fx-font-size: 42");
-        optionsMenu.setMinWidth(400);
+        optionsMenu.setMinWidth(600);
         optionsMenu.setStyle("-fx-font-size: 18pt; -fx-background-color: white; -fx-padding: 10px;");
         Welcome.setStyle("-fx-padding: 20,0,0,0; -fx-alignment: center; -fx-font-size: 30");
-        window.setScene(scene);
-        window.setMaximized(true);
-        window.show();
+       // window.setScene(scene);
+       // window.setMaximized(true);
+       // window.show();
 
     }
 }
