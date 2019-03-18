7d566d73872c8840606e097fd33167acaac7daa1
Hashim Karim <h.karim-2@student.tudelft.nl>
2019-03-18 13:13:36 +0000

    Revert "Merge branch 'GUI-TIJS-BRANCH' into 'master'"
    This reverts merge request !30

--- _2_client/src/main/java/client/Main.java
+++ _2_client/src/main/java/client/Main.java
@@ -9,7 +9,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -28,7 +27,6 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws FileNotFoundException {
 
-        primaryStage.getIcons().add(new Image("https://i.imgur.com/diKacP0.png"));
         window = primaryStage;
         window.setOnCloseRequest(e -> {
             e.consume();
