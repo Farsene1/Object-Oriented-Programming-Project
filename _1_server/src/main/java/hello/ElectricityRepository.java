a2cc42e2c3a4fbf96731a496d3770c04b1186e48
julianholle <48092782+julianholle@users.noreply.github.com>
2019-03-19 16:06:30 +0100

    Electricity Repository class

---
+++ _1_server/src/main/java/hello/ElectricityRepository.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ElectricityRepository extends CrudRepository<Electricity, Integer> {
+    @Query(value = "SELECT * from electricity", nativeQuery = true)
+    List<Electricity> findAllElectricity();
+
+    @Query(value = "SELECT * from electricity WHERE username = ?1", nativeQuery = true)
+    List<Electricity> findElectricityByUser(String username);
+}
+ssss
