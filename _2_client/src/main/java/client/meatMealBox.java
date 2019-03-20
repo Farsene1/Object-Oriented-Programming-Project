05511405413020fcc039b255b951b08924bf613d
olliepvp <ollieparfittpallandt@gmail.com>
2019-03-20 14:24:22 +0100

    add meatMealBox class for calculator

---
+++ _2_client/src/main/java/client/meatMealBox.java
@@ -0,0 +1,22 @@
+package client;
+import classes.Activity;
+import classes.Controller;
+import classes.User;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.control.ChoiceBox;
+import java.util.LinkedList;
+import java.util.List;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class meatMealBox {
+
+}
