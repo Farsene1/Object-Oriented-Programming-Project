9fb7a255d089d0c4f6a1544a37a1b1e29789c55f
TZijl <t.zijl@student.tudelft.nl>
2019-03-24 20:49:06 +0100

    reformat

--- _2_client/src/main/java/client/ElectricityBox.java
+++ _2_client/src/main/java/client/ElectricityBox.java
@@ -1,18 +1,17 @@
 package client;
 
-
 import classes.Activity;
 import classes.Controller;
 import classes.Electricity;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -25,9 +24,8 @@ public class ElectricityBox {
     static int total_hrs;
     static String date;
     static int score;
-    public static Electricity addUsage(String title, String message, classes.User user ){
-        TextField lightfield;
-        TextField  heatfield;
+
+    public static Electricity addUsage(String title, String message, classes.User user) {
         Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
@@ -40,6 +38,8 @@ public class ElectricityBox {
         ChoiceBox<String> choiceBox = new ChoiceBox<>();
         choiceBox.getItems().addAll("I have used solar energy", "I have NOT used solar energy");
         choiceBox.setValue("I have NOT used solar energy");
+        TextField lightfield;
+        TextField heatfield;
         lightfield = new TextField();
         lightfield.setMaxWidth(300);
         heatfield = new TextField();
@@ -47,48 +47,46 @@ public class ElectricityBox {
         Label lightlabel = new Label("Add your hours of light usage");
         Label heatlabel = new Label("Add your hours of heating usage");
         Button send = new Button("Submit");
-        send.setOnAction(e ->{
-            int c=0;
-            for(int i=0; i<lightfield.getText().length(); i++){
-                if((Character.isDigit(lightfield.getText().charAt(i)))){
+        send.setOnAction(e -> {
+            int c = 0;
+            for (int i = 0; i < lightfield.getText().length(); i++) {
+                if ((Character.isDigit(lightfield.getText().charAt(i)))) {
                     c++;
                 }
             }
-            int v=0;
-            for(int i=0; i<heatfield.getText().length(); i++){
-                if((Character.isDigit(heatfield.getText().charAt(i)))){
+            int v = 0;
+            for (int i = 0; i < heatfield.getText().length(); i++) {
+                if ((Character.isDigit(heatfield.getText().charAt(i)))) {
                     v++;
                 }
             }
 
-            if(c==lightfield.getText().length()&&v==heatfield.getText().length()){
+            if (c == lightfield.getText().length() && v == heatfield.getText().length()) {
                 LocalDateTime mydateObj = LocalDateTime.now();
                 DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-                date= mydateObj.format(myFormatObj);
+                date = mydateObj.format(myFormatObj);
                 solar = false;
-                if(choiceBox.getValue().equals("I have used solar energy")) {
+                if (choiceBox.getValue().equals("I have used solar energy")) {
                     solar = true;
                 }
                 light_hrs = Integer.parseInt(lightfield.getText());
                 heat_hrs = Integer.parseInt(heatfield.getText());
                 total_hrs = light_hrs + heat_hrs;
                 Electricity electricity = new Electricity(heat_hrs, light_hrs, solar);
-                if(solar) {
-                    score = 25*total_hrs;
-                }
-                else {
-                    score = 100*total_hrs;
+                if (solar) {
+                    score = 25 * total_hrs;
+                } else {
+                    score = 100 * total_hrs;
                 }
                 electricity.setScore(score);
                 new Controller().sendTransport(user, score);
                 // add electricity in the database
-                Activity activity = new Activity(user.getUsername(),3,"Electricity usage: "+total_hrs+" Hours",
+                Activity activity = new Activity(user.getUsername(), 3, "Electricity usage: " + total_hrs + " Hours",
                         electricity.getScore(), date);
                 List<Activity> list = new Controller().sendFood(activity);
-                System.out.println("\n The items are"+list.toString());
+                System.out.println("\n The items are" + list.toString());
                 window.close();
-            }
-            else{
+            } else {
                 lightfield.clear();
                 heatfield.clear();
                 errorlabel.setVisible(true);
@@ -96,20 +94,17 @@ public class ElectricityBox {
         });
 
 
-        VBox layout= new VBox(10);
+        VBox layout = new VBox(10);
 
-        layout.getChildren().addAll(label,choiceBox,lightlabel,lightfield,heatlabel,heatfield,errorlabel,send);
+        layout.getChildren().addAll(label, choiceBox, lightlabel, lightfield, heatlabel, heatfield, errorlabel, send);
         layout.setAlignment(Pos.CENTER);
-        Scene scene= new Scene(layout);
+        Scene scene = new Scene(layout);
         window.setScene(scene);
         window.showAndWait();
 
 
-
         return new Electricity(heat_hrs, light_hrs, solar);
     }
-
-
 
 
 }
