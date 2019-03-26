c30fb23124305e76b1151c29ab0b42e9b44d5c62
julianholle <48092782+julianholle@users.noreply.github.com>
2019-03-26 15:36:55 +0100

    no message

--- _1_server/src/main/java/hello/StatisticsController.java
+++ _1_server/src/main/java/hello/StatisticsController.java
@@ -3,8 +3,6 @@ package hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 
 /**
  * This is the Rest Controller.
