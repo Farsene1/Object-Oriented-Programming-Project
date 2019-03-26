7a5b476ea68540d3ef85080b7eebaffc01203ffd
Hashim Kaim <hashimkarim168@gmail.com>
2019-03-26 23:54:09 +0100

    Fixed the achievements class setup

--- _2_client/src/main/java/client/Achievements.java
+++ _2_client/src/main/java/client/Achievements.java
@@ -37,7 +37,7 @@ public class Achievements {
 
         Label myAchievements = new Label("My Achievements!");
         myAchievements.setFont(Font.font("Amble CN", FontWeight.BOLD, 35));
-        VBox achievements = new VBox(myAchievements);
+        VBox achievements = new VBox();
         achievements.setStyle("-fx-padding: 5;");
         achievements.setSpacing(10);
         achievements.setAlignment(Pos.CENTER);
@@ -48,9 +48,10 @@ public class Achievements {
          */
         HBox polarHbox = new HBox();
         polarHbox.setSpacing(1);
-        polarHbox.setStyle("-fx-padding: 1;" + "-fx-border-style: solid inside;"
+        achievements.setStyle("-fx-padding: 1;" + "-fx-border-style: solid inside;"
                 + "-fx-border-width: 5;" + "-fx-border-insets: 5;"
-                + "-fx-border-radius: 5;" + "-fx-border-color: #6dfff3;");
+                + "-fx-border-radius: 5;" + "-fx-border-color: #6dfff3;" +
+                "-fx-background-color: rgba(255,255,255,0.4)");
 
         Button polarBadge_1 = new Button();
         Button polarBadge_2 = new Button();
@@ -76,31 +77,13 @@ public class Achievements {
 
         Label polarLabel = new Label("POLAR");
         polarLabel.setFont(Font.font("Amble CN", FontWeight.BOLD, 20));
-        HBox polarAchievements = new HBox(polarLabel);
-        polarAchievements.setStyle("-fx-padding: 5;");
-        polarAchievements.setSpacing(10);
-        polarAchievements.setAlignment(Pos.CENTER_LEFT);
-        VBox polar_1 = new VBox(polarBadge_1);
-        VBox polar_2 = new VBox(polarBadge_2);
-        VBox polar_3 = new VBox(polarBadge_3);
-        VBox polar_4 = new VBox(polarBadge_4);
-        VBox polar_5 = new VBox(polarBadge_5);
-        VBox polar_6 = new VBox(polarBadge_6);
-        VBox polar_7 = new VBox(polarBadge_7);
-        VBox polar_8 = new VBox(polarBadge_8);
-        VBox polar_9 = new VBox(polarBadge_9);
-        VBox polar_10 = new VBox(polarBadge_10);
-        polarHbox.getChildren().addAll(polarAchievements, polar_1, polar_2, polar_3, polar_4, polar_5, polar_6, polar_7, polar_8, polar_9, polar_10);
-        GridPane.setConstraints(polarHbox, 0, 2);
+        polarHbox.getChildren().addAll(polarBadge_1, polarBadge_2, polarBadge_3, polarBadge_4, polarBadge_5,
+                polarBadge_6, polarBadge_7, polarBadge_8, polarBadge_9, polarBadge_10);
 
         /**
          * Food score badges
          */
         HBox foodHbox = new HBox();
-        foodHbox.setSpacing(1);
-        foodHbox.setStyle("-fx-padding: 1;" + "-fx-border-style: solid inside;"
-                + "-fx-border-width: 5;" + "-fx-border-insets: 5;"
-                + "-fx-border-radius: 5;" + "-fx-border-color: #44ff8e;");
 
         Button foodBadge_1 = new Button();
         Button foodBadge_2 = new Button();
@@ -126,32 +109,15 @@ public class Achievements {
 
         Label foodLabel = new Label("FOOD");
         foodLabel.setFont(Font.font("Amble CN", FontWeight.BOLD, 20));
-        HBox foodAchievements = new HBox(foodLabel);
-        foodAchievements.setStyle("-fx-padding: 5;");
-        foodAchievements.setSpacing(10);
-        foodAchievements.setAlignment(Pos.CENTER_LEFT);
 
-        VBox food_1 = new VBox(foodBadge_1);
-        VBox food_2 = new VBox(foodBadge_2);
-        VBox food_3 = new VBox(foodBadge_3);
-        VBox food_4 = new VBox(foodBadge_4);
-        VBox food_5 = new VBox(foodBadge_5);
-        VBox food_6 = new VBox(foodBadge_6);
-        VBox food_7 = new VBox(foodBadge_7);
-        VBox food_8 = new VBox(foodBadge_8);
-        VBox food_9 = new VBox(foodBadge_9);
-        VBox food_10 = new VBox(foodBadge_10);
-        foodHbox.getChildren().addAll(foodAchievements, food_1, food_2, food_3, food_4, food_5, food_6, food_7, food_8, food_9, food_10);
-        GridPane.setConstraints(foodHbox, 0, 4);
+        foodHbox.getChildren().addAll(foodBadge_1, foodBadge_2, foodBadge_3, foodBadge_4, foodBadge_5,
+                foodBadge_6, foodBadge_7, foodBadge_8, foodBadge_9, foodBadge_10);
 
         /**
          * transport score badges
          */
         HBox transportHbox = new HBox();
         transportHbox.setSpacing(1);
-        transportHbox.setStyle("-fx-padding: 1;" + "-fx-border-style: solid inside;"
-                + "-fx-border-width: 5;" + "-fx-border-insets: 5;"
-                + "-fx-border-radius: 5;" + "-fx-border-color: #ff362c;");
 
         Button transportBadge_1 = new Button();
         Button transportBadge_2 = new Button();
@@ -177,23 +143,9 @@ public class Achievements {
 
         Label transportLabel = new Label("TRANSPORT");
         transportLabel.setFont(Font.font("Amble CN", FontWeight.BOLD, 20));
-        HBox transportAchievements = new HBox(transportLabel);
-        transportAchievements.setStyle("-fx-padding: 5;");
-        transportAchievements.setSpacing(10);
-        transportAchievements.setAlignment(Pos.CENTER_LEFT);
 
-        VBox transport_1 = new VBox(transportBadge_1);
-        VBox transport_2 = new VBox(transportBadge_2);
-        VBox transport_3 = new VBox(transportBadge_3);
-        VBox transport_4 = new VBox(transportBadge_4);
-        VBox transport_5 = new VBox(transportBadge_5);
-        VBox transport_6 = new VBox(transportBadge_6);
-        VBox transport_7 = new VBox(transportBadge_7);
-        VBox transport_8 = new VBox(transportBadge_8);
-        VBox transport_9 = new VBox(transportBadge_9);
-        VBox transport_10 = new VBox(transportBadge_10);
-        transportHbox.getChildren().addAll(transportAchievements, transport_1, transport_2, transport_3, transport_4, transport_5, transport_6, transport_7, transport_8, transport_9, transport_10);
-        GridPane.setConstraints(transportHbox, 0, 8);
+        transportHbox.getChildren().addAll(transportBadge_1, transportBadge_2, transportBadge_3, transportBadge_4, transportBadge_5,
+                transportBadge_6, transportBadge_7, transportBadge_8, transportBadge_9, transportBadge_10);
 
 
 
@@ -202,9 +154,6 @@ public class Achievements {
  */
         HBox electricityHbox = new HBox();
         electricityHbox.setSpacing(1);
-        electricityHbox.setStyle("-fx-padding: 1;" + "-fx-border-style: solid inside;"
-                + "-fx-border-width: 5;" + "-fx-border-insets: 5;"
-                + "-fx-border-radius: 5;" + "-fx-border-color: #f7ff28;");
 
         Button electricityBadge_1 = new Button();
         Button electricityBadge_2 = new Button();
@@ -230,25 +179,14 @@ public class Achievements {
 
         Label electricityLabel = new Label("ELECTRICITY");
         electricityLabel.setFont(Font.font("Amble CN", FontWeight.BOLD, 20));
-        HBox electricityAchievements = new HBox(electricityLabel);
-        electricityAchievements.setStyle("-fx-padding: 5;");
-        electricityAchievements.setSpacing(10);
-        electricityAchievements.setAlignment(Pos.CENTER_LEFT);
 
-        VBox electricity_1 = new VBox(electricityBadge_1);
-        VBox electricity_2 = new VBox(electricityBadge_2);
-        VBox electricity_3 = new VBox(electricityBadge_3);
-        VBox electricity_4 = new VBox(electricityBadge_4);
-        VBox electricity_5 = new VBox(electricityBadge_5);
-        VBox electricity_6 = new VBox(electricityBadge_6);
-        VBox electricity_7 = new VBox(electricityBadge_7);
-        VBox electricity_8 = new VBox(electricityBadge_8);
-        VBox electricity_9 = new VBox(electricityBadge_9);
-        VBox electricity_10 = new VBox(electricityBadge_10);
-        electricityHbox.getChildren().addAll(electricityAchievements, electricity_1, electricity_2, electricity_3, electricity_4, electricity_5, electricity_6, electricity_7, electricity_8, electricity_9, electricity_10);
-        GridPane.setConstraints(electricityHbox, 0, 10);
+        electricityHbox.getChildren().addAll(electricityBadge_1, electricityBadge_2, electricityBadge_3, electricityBadge_4, electricityBadge_5,
+                electricityBadge_6, electricityBadge_7, electricityBadge_8, electricityBadge_9, electricityBadge_10);
 
-        grid.getChildren().addAll(achievements, polarHbox, foodHbox, transportHbox, electricityHbox);
+        achievements.getChildren().addAll(myAchievements,polarLabel, polarHbox,foodLabel, foodHbox,transportLabel, transportHbox,electricityLabel, electricityHbox);
+
+        grid.getChildren().addAll(achievements);
+        achievements.setAlignment(Pos.CENTER);
 
         grid.setMinWidth(1500);
         grid.setStyle("-fx-font-size: 18pt; -fx-padding: 10px;");
