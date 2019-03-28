dfe05152967f1f23d248b40584517631eb40f5d7
olliepvp <ollieparfittpallandt@gmail.com>
2019-03-28 12:59:20 +0100

    badges working and changing on selection!

--- _2_client/src/main/java/classes/UserBadge.java
+++ _2_client/src/main/java/classes/UserBadge.java
@@ -8,11 +8,14 @@ public class UserBadge {
 
     static public String iconChoice(int number){
 
-        if(number == 2){
-          icon = "https://i.ibb.co/Sn20yTR/polar1.png";
-          System.out.println("2 sleected");
+        if(number == 1){
+          icon = "https://drive.google.com/uc?id=17b0fCmDcAogmyqUERNk7bMBOCRxwnYd_";
+          System.out.println("1 selected");
         }
-
+        if(number == 2){
+            icon = "https://drive.google.com/uc?id=1QV1p0s_JHtBfTx8-Vh_LgbucCgYWULrp";
+            System.out.println("2 selected");
+        }
     return icon;
 }
 
