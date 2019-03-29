532a141a00a4af7bfe5cea0693de86c0bce4bc2e
julianholle <48092782+julianholle@users.noreply.github.com>
2019-03-29 14:26:50 +0100

    Statistics server-side

--- _1_server/src/main/java/hello/ActivityRepository.java
+++ _1_server/src/main/java/hello/ActivityRepository.java
@@ -25,28 +25,28 @@ public interface ActivityRepository extends CrudRepository<Activity, Integer> {
     List<Activity> findActivitiesByUser(String username);
 
     /**
-     * @return total food footprint of a single user on a specific day.
-     */
-    @Query(value = "SELECT SUM(footprint) FROM activities WHERE username=?1 AND date=?2 AND category=1", nativeQuery = true)
-    Integer totalFoodFootprint(String username, String date);
-
-    /**
-     * @return total transport footprint of a single user on a specific day.
-     */
-    @Query(value = "SELECT SUM(footprint) FROM activities WHERE username=?1 AND date=?2 AND category=2", nativeQuery = true)
-    Integer totalTransportFootprint(String username, String date);
-
-    /**
-     * @return total electricity footprint of a single user on a specific day.
-     */
-    @Query(value = "SELECT SUM(footprint) FROM activities WHERE username=?1 AND date=?2 AND category=3", nativeQuery = true)
-    Integer totalElectricityFootprint(String username, String date);
-
-    /**
      * @return total footprint of a single user on a specific day.
      */
     @Query(value = "SELECT SUM(footprint) FROM activities WHERE username=?1 AND date=?2", nativeQuery = true)
     Integer totalFootprint(String username, String date);
+
+    //    /**
+//     * @return total food footprint of a single user on a specific day.
+//     */
+//    @Query(value = "SELECT SUM(footprint) FROM activities WHERE username=?1 AND date=?2 AND category=1", nativeQuery = true)
+//    Integer totalFoodFootprint(String username, String date);
+//
+//    /**
+//     * @return total transport footprint of a single user on a specific day.
+//     */
+//    @Query(value = "SELECT SUM(footprint) FROM activities WHERE username=?1 AND date=?2 AND category=2", nativeQuery = true)
+//    Integer totalTransportFootprint(String username, String date);
+//
+//    /**
+//     * @return total electricity footprint of a single user on a specific day.
+//     */
+//    @Query(value = "SELECT SUM(footprint) FROM activities WHERE username=?1 AND date=?2 AND category=3", nativeQuery = true)
+//    Integer totalElectricityFootprint(String username, String date);
 
 }
 
--- _1_server/src/main/java/hello/StatisticsController.java
+++ _1_server/src/main/java/hello/StatisticsController.java
@@ -22,40 +22,11 @@ public class StatisticsController {
     @Autowired
     private ActivityRepository activityRepository;
 
-    /**
-     * addTotalFoodFootprint
-     */
-    @RequestMapping(value = "/totalFood", method = RequestMethod.POST)
-    public Integer addFoodToStatisticsTable(@RequestBody Statistics statistics) {
-        this.statisticsRepository.save(statistics);
-        System.out.println("statistics table updates");
-        return this.activityRepository.totalFoodFootprint(statistics.getUsername(), statistics.getDate());
-    }
-
-    /**
-     * addTotalTransportFootprint
-     */
-    @RequestMapping(value = "/totalTransport", method = RequestMethod.POST)
-    public Integer addTransportToStatisticsTable(@RequestBody Statistics statistics) {
-        this.statisticsRepository.save(statistics);
-        System.out.println("statistics table updates");
-        return this.activityRepository.totalTransportFootprint(statistics.getUsername(), statistics.getDate());
-    }
-
-    /**
-     * addTotalElectricityFootprint
-     */
-    @RequestMapping(value = "/totalElectricity", method = RequestMethod.POST)
-    public Integer addElectricityToStatisticsTable(@RequestBody Statistics statistics) {
-        this.statisticsRepository.save(statistics);
-        System.out.println("statistics table updates");
-        return this.activityRepository.totalElectricityFootprint(statistics.getUsername(), statistics.getDate());
-    }
 
     /**
      * addTotalFootprint
      */
-    @RequestMapping(value = "/total", method = RequestMethod.POST)
+    @RequestMapping(value = "/totalFootprint", method = RequestMethod.POST)
     public Integer addTotalFootprintToStatisticsTable(@RequestBody Statistics statistics) {
         this.statisticsRepository.save(statistics);
         System.out.println("statistics table updates");
@@ -71,3 +42,34 @@ public class StatisticsController {
         this.statisticsRepository = statisticsRepository;
     }
 }
+
+//    /**
+//     * addTotalFoodFootprint
+//     */
+//    @RequestMapping(value = "/totalFood", method = RequestMethod.POST)
+//    public Integer addFoodToStatisticsTable(@RequestBody Statistics statistics) {
+//        this.statisticsRepository.save(statistics);
+//        System.out.println("statistics table updates");
+//        return this.activityRepository.totalFoodFootprint(statistics.getUsername(), statistics.getDate());
+//    }
+//
+//    /**
+//     * addTotalTransportFootprint
+//     */
+//    @RequestMapping(value = "/totalTransport", method = RequestMethod.POST)
+//    public Integer addTransportToStatisticsTable(@RequestBody Statistics statistics) {
+//        this.statisticsRepository.save(statistics);
+//        System.out.println("statistics table updates");
+//        return this.activityRepository.totalTransportFootprint(statistics.getUsername(), statistics.getDate());
+//    }
+//
+//    /**
+//     * addTotalElectricityFootprint
+//     */
+//    @RequestMapping(value = "/totalElectricity", method = RequestMethod.POST)
+//    public Integer addElectricityToStatisticsTable(@RequestBody Statistics statistics) {
+//        this.statisticsRepository.save(statistics);
+//        System.out.println("statistics table updates");
+//        return this.activityRepository.totalElectricityFootprint(statistics.getUsername(), statistics.getDate());
+//    }
+
