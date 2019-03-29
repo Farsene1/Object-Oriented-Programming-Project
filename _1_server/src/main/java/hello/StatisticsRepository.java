8f66fbea886d3a6aac37516d07572616ff4abb57
julianholle <48092782+julianholle@users.noreply.github.com>
2019-03-29 15:04:20 +0100

    repository

--- _1_server/src/main/java/hello/StatisticsRepository.java
+++ _1_server/src/main/java/hello/StatisticsRepository.java
@@ -3,9 +3,15 @@ package hello;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * interface for hibernate.
  */
 public interface StatisticsRepository extends CrudRepository<Statistics, Integer> {
-    
+    /**
+     * Query to findAllStatistics.
+     */
+    @Query(value = "SELECT * from statistics", nativeQuery = true)
+    List<Statistics> findAllStatistics();
 }
