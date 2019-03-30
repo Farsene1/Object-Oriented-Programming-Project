d927eb26ec3f96db762528273f7c1bedadf031bb
Florentin Arsene <F.Arsene@student.tudelft.nl>
2019-03-30 11:17:02 +0100

    test

--- _1_server/src/test/java/FriendshipControllerTest.java
+++ _1_server/src/test/java/FriendshipControllerTest.java
@@ -5,6 +5,9 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FriendshipControllerTest {
@@ -26,16 +29,16 @@ public class FriendshipControllerTest {
         friendshipController.setFriendshipRepository(friendshipRepository);
         friendshipController.setUserRepository(userRepository);
     }
-//
-//    @Test
-//    public void getAllFriendsTest(){
-//        Mockito.when(friendshipRepository.getAllFriends("admin")).thenReturn(Arrays.asList("friend1","friend2"));
-//        Mockito.when(userRepository.findUserByUsername("friend1")).thenReturn(Arrays.asList(new User("friend1","hash1")));
-//        Mockito.when(userRepository.findUserByUsername("friend2")).thenReturn(Arrays.asList(new User("friend2","hash2")));
-//        List<User> list = friendshipController.getAllFriends("admin");
-//        assertEquals(2,list.size());
-//    }
-//
+
+    @Test
+    public void getAllFriendsTest(){
+        Mockito.when(friendshipRepository.getAllFriends("admin")).thenReturn(Arrays.asList("friend1","friend2"));
+        Mockito.when(userRepository.findUserByUsername("friend1")).thenReturn(Arrays.asList(new User("friend1","hash1")));
+        Mockito.when(userRepository.findUserByUsername("friend2")).thenReturn(Arrays.asList(new User("friend2","hash2")));
+        List<User> list = friendshipController.getAllFriends(new User("admin","hash"));
+        assertEquals(2,list.size());
+    }
+
     @Test
     public void makeRequestTest(){
         FriendRequest f = new FriendRequest("a","b");
