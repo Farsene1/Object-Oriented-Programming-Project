21a79f2283983567ab1bcbd248769e68bf648d4b
Florentin Arsene <F.Arsene@student.tudelft.nl>
2019-03-30 11:27:43 +0100

    test

--- _1_server/src/test/java/FriendshipControllerTest.java
+++ _1_server/src/test/java/FriendshipControllerTest.java
@@ -24,25 +24,42 @@ public class FriendshipControllerTest {
     private UserRepository userRepository = Mockito.mock(UserRepository.class);
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         friendshipController.setFriendRequestRepository(friendRequestRepository);
         friendshipController.setFriendshipRepository(friendshipRepository);
         friendshipController.setUserRepository(userRepository);
     }
 
     @Test
-    public void getAllFriendsTest(){
-        Mockito.when(friendshipRepository.getAllFriends("admin")).thenReturn(Arrays.asList("friend1","friend2"));
-        Mockito.when(userRepository.findUserByUsername("friend1")).thenReturn(Arrays.asList(new User("friend1","hash1")));
-        Mockito.when(userRepository.findUserByUsername("friend2")).thenReturn(Arrays.asList(new User("friend2","hash2")));
-        List<User> list = friendshipController.getAllFriends(new User("admin","hash"));
-        assertEquals(2,list.size());
+    public void getAllFriendsTest() {
+        Mockito.when(friendshipRepository.getAllFriends("admin")).thenReturn(Arrays.asList("friend1", "friend2"));
+        Mockito.when(userRepository.findUserByUsername("friend1")).thenReturn(Arrays.asList(new User("friend1", "hash1")));
+        Mockito.when(userRepository.findUserByUsername("friend2")).thenReturn(Arrays.asList(new User("friend2", "hash2")));
+        List<User> list = friendshipController.getAllFriends(new User("admin", "hash"));
+        assertEquals(2, list.size());
     }
 
     @Test
-    public void makeRequestTest(){
-        FriendRequest f = new FriendRequest("a","b");
-        assertEquals("SENT",friendshipController.makeRequest(f));
+    public void getAllRequestsTest() {
+        Mockito.when(friendRequestRepository.findAllRequestsFor("admin")).thenReturn(Arrays.asList(new FriendRequest(), new FriendRequest()));
+        List<FriendRequest> list = friendshipController.getAllRequest(new User("admin","root"));
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void makeRequestTest() {
+        FriendRequest f = new FriendRequest("a", "b");
+        assertEquals("SENT", friendshipController.makeRequest(f));
+    }
+
+    @Test
+    public void respondTest() {
+        assertEquals("OK", friendshipController.respond(new FriendRequest()));
+    }
+
+    @Test
+    public void fakeRespondTest() {
+        assertEquals("OK", friendshipController.fakeRespond(new FriendRequest()));
     }
 
 }
