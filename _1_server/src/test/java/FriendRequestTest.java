d5848dd5a5105894078e71c55243b016984de079
Hashim Karim <h.karim-2@student.tudelft.nl>
2019-03-31 21:52:19 +0000

    Revert "Revert "Merge branch 'Statistics' into 'GUI-DEV'""
    This reverts commit df8a3eb0243176220d1806defa646c1a562d9060

--- _1_server/src/test/java/FriendRequestTest.java
+++ _1_server/src/test/java/FriendRequestTest.java
@@ -1,2 +1,26 @@
+import hello.FriendRequest;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class FriendRequestTest {
+    FriendRequest friendRequest;
+
+    @Before
+    public void setUp(){
+        friendRequest = new FriendRequest("Julian","Hashim");
+    }
+
+    @Test
+    public void constrTest(){assertEquals(true, friendRequest != null);}
+
+    @Test
+    public void getSetTest(){
+        assertEquals("Julian", friendRequest.getSender());
+        assertEquals("Hashim", friendRequest.getReceiver());
+        friendRequest.setSender(friendRequest.getSender());
+        friendRequest.setReceiver(friendRequest.getReceiver());
+    }
 }
