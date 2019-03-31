13666c75085f92c01d2b607baccc2474171dc4d5
julianholle <48092782+julianholle@users.noreply.github.com>
2019-03-31 22:41:48 +0200

    Testing

--- _1_server/src/test/java/FriendshipTest.java
+++ _1_server/src/test/java/FriendshipTest.java
@@ -16,4 +16,21 @@ public class FriendshipTest {
 
     @Test
     public void constrTest(){assertEquals(true, friendship != null);}
+
+    @Test
+    public void getSetTest(){
+        assertEquals("Julian", friendship.getUsername1());
+        assertEquals("Flo", friendship.getUsername2());
+        friendship.setUsername1(friendship.getUsername1());
+        friendship.setUsername2(friendship.getUsername1());
+    }
+
+    @Test
+    public void toStringTest(){
+        Friendship f = new Friendship("","");
+        f.setId(1);
+        assertEquals(f.getId()+0,1);
+
+        assertTrue(friendship.toString().contains("Julian"));
+    }
 }
