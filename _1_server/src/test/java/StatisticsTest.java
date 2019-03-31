65eab5f41bce0197607d500dcf35806102f08197
julianholle <48092782+julianholle@users.noreply.github.com>
2019-03-31 22:13:37 +0200

    Testing

---
+++ _1_server/src/test/java/StatisticsTest.java
@@ -0,0 +1,10 @@
+import hello.Statistics;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StatisticsTest {
+    Statistics statistics;
+}
