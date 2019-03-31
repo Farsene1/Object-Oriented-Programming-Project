d538145ac5816252aea1862748ef34444e11c973
julianholle <48092782+julianholle@users.noreply.github.com>
2019-03-31 22:35:06 +0200

    Testing

--- _1_server/src/test/java/FriendshipTest.java
+++ _1_server/src/test/java/FriendshipTest.java
@@ -1,2 +1,11 @@
+import hello.Friendship;
+import hello.Statistics;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class FriendshipTest {
+    private Friendship friendship;
 }
