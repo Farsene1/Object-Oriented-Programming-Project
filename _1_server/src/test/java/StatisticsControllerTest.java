1f99824a1fd5e8a83c7624f28216dd11f3965d9c
julianholle <48092782+julianholle@users.noreply.github.com>
2019-03-31 22:20:49 +0200

    Testing

---
+++ _1_server/src/test/java/StatisticsControllerTest.java
@@ -0,0 +1,19 @@
+import hello.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class StatisticsControllerTest {
+
+    @Autowired
+    private StatisticsController controller = new StatisticsController();
+}
