b1c622170e8467baac1e7e13ca2e2ab36679f622
Stelios Avgousti <s.avgousti@student.tudelft.nl>
2019-04-04 21:19:58 +0200

    food transport  and polar left. checkstyling classes.

--- _2_client/src/main/java/CSS/CSS.java
+++ _2_client/src/main/java/CSS/CSS.java
@@ -1,24 +1,57 @@
 package CSS;
 
-import com.jfoenix.controls.JFXSnackbar;
+
 import javafx.beans.binding.Bindings;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+/**
+ * CSS class.
+ */
 public class CSS {
-    public static void setButtonStyle(Button button){
+    /**
+     * Sets button style.
+     *
+     * @param button buttom parameter.
+     */
+    public static void setButtonStyle(final Button button) {
         button.styleProperty().bind(Bindings.when(button.hoverProperty())
-                .then("-fx-pref-width: 400;-fx-pref-height: 70;-fx-font-size: 42; -fx-background-color: #81b8d7; -fx-text-fill: White;-fx-border-color: white;")
-                .otherwise("-fx-pref-width: 400;-fx-pref-height: 70;-fx-font-size: 42; -fx-background-color: #a8d2e9; -fx-text-fill: black;-fx-border-color: white;"));
+                .then("-fx-pref-width: 400;"
+                        + "-fx-pref-height: 70;-fx-font-size: 42;"
+                        + " -fx-background-color: #81b8d7;"
+                        + " -fx-text-fill: White;"
+                        + "-fx-border-color: white;")
+                .otherwise("-fx-pref-width: 400;"
+                        + "-fx-pref-height: 70;"
+                        + "-fx-font-size: 42;"
+                        + " -fx-background-color: #a8d2e9;"
+                        + " -fx-text-fill: black;"
+                        + "-fx-border-color: white;"));
     }
 
-    public static void setSmallButtonStyle(Button button){
+    /**
+     * Sets small button style.
+     *
+     * @param button small button parameter.
+     */
+    public static void setSmallButtonStyle(final Button button) {
         button.styleProperty().bind(Bindings.when(button.hoverProperty())
-                .then("-fx-background-color: #81b8d7; -fx-text-fill: white;-fx-border-color:rgba(0,0,0,0) ;-fx-background-radius: 10;")
-                .otherwise("-fx-background-color: #a8d2e9; -fx-text-fill: white;-fx-border-color: rgba(0,0,0,0);-fx-background-radius: 10;"));
+                .then("-fx-background-color: #81b8d7;"
+                        + " -fx-text-fill: white;"
+                        + "-fx-border-color:rgba(0,0,0,0) ;"
+                        + "-fx-background-radius: 10;")
+                .otherwise("-fx-background-color: #a8d2e9;"
+                        + " -fx-text-fill: white;"
+                        + "-fx-border-color: rgba(0,0,0,0);"
+                        + "-fx-background-radius: 10;"));
     }
 
-    public static void setBackButtonStyle(Button button){
+    /**
+     * Sets back button style.
+     *
+     * @param button button parameter.
+     */
+    public static void setBackButtonStyle(final Button button) {
         button.styleProperty().bind(Bindings.when(button.hoverProperty())
                 .then("-fx-graphic: url('https://i.ibb.co/dgvMZgJ/back-hover-small.png'); " +
                         "-fx-background-color: rgba(0,0,0,0); -fx-border-color: rgba(0,0,0,0);")
@@ -26,14 +59,25 @@ public class CSS {
                         "-fx-background-color: rgba(0,0,0,0); -fx-border-color: rgba(0,0,0,0);"));
     }
 
-    public static void setTrophyButtonStyle(Button button){
+    /**
+     * Sets trohpy button style.
+     *
+     * @param button trophy button
+     */
+    public static void setTrophyButtonStyle(final Button button) {
         button.styleProperty().bind(Bindings.when(button.hoverProperty())
                 .then("-fx-graphic: url('https://i.ibb.co/Jxsmw99/trophy-hover.png'); " +
                         "-fx-background-color: rgba(0,0,0,0); -fx-border-color: rgba(0,0,0,0);")
                 .otherwise("-fx-graphic: url('https://i.ibb.co/YcCNG8K/trophy.png'); " +
                         "-fx-background-color: rgba(0,0,0,0); -fx-border-color: rgba(0,0,0,0);"));
     }
-    public static void setTextFieldStyle(TextField textField){
+
+    /**
+     * Seets text field style.
+     *
+     * @param textField textfield parameter.
+     */
+    public static void setTextFieldStyle(final TextField textField) {
         textField.styleProperty().bind(Bindings.when(textField.hoverProperty())
                 .then("")
                 .otherwise(""));
--- _2_client/src/main/java/CSS/CSSTable.java
+++ _2_client/src/main/java/CSS/CSSTable.java
@@ -3,9 +3,15 @@ package CSS;
 
 import javafx.scene.control.TableView;
 
-public class CSSTable
-{
-    public static void styleTable(TableView table){
+/**
+ * CSS TABLE.
+ */
+public class CSSTable {
+    /**
+     * TABLE VIEW STYLING.
+     * @param table tabvle parameter.
+     */
+    public static void styleTable(final TableView table) {
         table.setStyle("-fx-background-color: rgba(173,216,230 ,0.3 )");
     }
 
