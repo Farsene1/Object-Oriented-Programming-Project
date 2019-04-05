5181ce321ec4e242bc0fa78faeb5a13b439ec68d
olliepvp <ollieparfittpallandt@gmail.com>
2019-04-05 23:32:04 +0200

    starting pie chart

--- _2_client/src/main/java/client/PieChart.java
+++ _2_client/src/main/java/client/PieChart.java
@@ -1,4 +1,37 @@
 package client;
 
-public class PieChart {
+        import javafx.application.Application;
+        import javafx.scene.Scene;
+        import javafx.scene.chart.PieChart;
+        import javafx.scene.layout.HBox;
+        import javafx.scene.layout.VBox;
+        import javafx.stage.Stage;
+
+        public class PieChart{
+
+    /**
+     * Declare Hbox.
+     */
+    static HBox pieChartBox;
+
+
+    @Override
+    public void PieChart() {
+        //primaryStage.setTitle("My First JavaFX App");
+
+        PieChart pieChart = new PieChart();
+
+        PieChart.Data slice1 = new PieChart.Data("Desktop", 213);
+
+        
+
+        }
+
+    /**
+     * getter for for electricityHbox.
+     */
+    public static HBox getPieChartBox() {
+        return pieChartBox;
+    }
+
 }
